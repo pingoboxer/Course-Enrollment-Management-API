83b3549c07f4452c02bbfe16abb4f6f7fa51ba53
Kpamor-Raphael <terngukpamor@gmail.com>
2026-02-18 17:38:48 +0100

    Added the course service router in main

---
+++ app/api/v1/courses.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter, status
+from typing import List
+from app.schemas.course_schema import (
+    CourseCreate,
+    CourseUpdate,
+    CourseResponse
+)
+from app.services.course_service import CourseService
+
+router = APIRouter(prefix="/courses", tags=["Courses"])
+
+
+# Public Access
+
+@router.get("", response_model=List[CourseResponse], status_code=status.HTTP_200_OK)
+def get_all_courses():
+    return CourseService.get_all_courses()
+
+
+@router.get("/{course_id}", response_model=CourseResponse, status_code=status.HTTP_200_OK)
+def get_course(course_id: int):
+    return CourseService.get_course_by_id(course_id)
+
+
+
+# Admin Only
+
+@router.post("", response_model=CourseResponse, status_code=status.HTTP_201_CREATED)
+def create_course(request: CourseCreate):
+    return CourseService.create_course(
+        title=request.title,
+        code=request.code,
+        role=request.role
+    )
+
+
+@router.put("/{course_id}", response_model=CourseResponse, status_code=status.HTTP_200_OK)
+def update_course(course_id: int, request: CourseUpdate):
+    return CourseService.update_course(
+        course_id=course_id,
+        title=request.title,
+        code=request.code,
+        role=request.role
+    )
+
+
+@router.delete("/{course_id}", status_code=status.HTTP_200_OK)
+def delete_course(course_id: int, role: str):
+    return CourseService.delete_course(course_id, role)
