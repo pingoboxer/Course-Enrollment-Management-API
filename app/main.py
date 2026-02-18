39ef2eab9f2b6baa32c03998fe33d221fac3f22b
Kpamor-Raphael <terngukpamor@gmail.com>
2026-02-18 20:24:19 +0100

    Setting up the main file to run the application

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from app.api.v1 import enrollments, users, courses
 
-app = FastAPI()
+app = FastAPI(
+    title="Course Enrollment Management API",
+    description="A simple API for managing course enrollments with role-based access control.",
+    version="1.0.0"
+)
 
 app.include_router(enrollments.router)
 app.include_router(users.router)
 app.include_router(courses.router)
 
-@app.get("/", status_code=200)
+@app.get("/", status_code=200, tags=["Health"])
 async def health_check():
     return {"status": "API is running"}
